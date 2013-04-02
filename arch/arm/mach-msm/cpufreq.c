20d903c596c0b8c03f40ca1ff6812cffff838e04
carvsdriver <carvsdriver@gmail.com>
2013-04-01 20:48:10 -0400

    cpufreq: *facepalm*

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -45,7 +45,6 @@ struct cpufreq_work_struct {
 /* Because we are hotplugging CPU1 using mpdecision, init should
    not change min/max after initial fixup to accomodate user changes */
 int cpuinitcount = 0;
-#define CONFIG_NR_CPUS = 2;
 #endif
 
 static DEFINE_PER_CPU(struct cpufreq_work_struct, cpufreq_work);
@@ -361,14 +360,14 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 	table = cpufreq_frequency_get_table(policy->cpu);
 	if (cpufreq_frequency_table_cpuinfo(policy, table)) {
 #ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
-		if (cpuintcount < CONFIG_NR_CPUS ) {
+		if (cpuinitcount < CONFIG_NR_CPUS ) {
 			policy->cpuinfo.min_freq = CONFIG_MSM_CPU_FREQ_MIN;
 			policy->cpuinfo.max_freq = CONFIG_MSM_CPU_FREQ_MAX;
 		}
 #endif
 	}
 #ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
-	if (cpuintcount < CONFIG_NR_CPUS ) {
+	if (cpuinitcount < CONFIG_NR_CPUS ) {
 		policy->min = CONFIG_MSM_CPU_FREQ_MIN;
 		policy->max = CONFIG_MSM_CPU_FREQ_MAX;
 	}
