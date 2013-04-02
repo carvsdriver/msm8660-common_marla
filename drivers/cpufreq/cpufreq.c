3300cbcc1d3939cb589f2d4da0c1d21945663eea
carvsdriver <carvsdriver@gmail.com>
2013-04-01 22:58:28 -0400

    Revert "cpufreq: store and recover user frequency policy on cpu core hotplug"
    This reverts commit 2bc23c030ebb962fe278a33dbfd2b351e009cc88.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -45,7 +45,7 @@ static DEFINE_PER_CPU(struct cpufreq_policy *, cpufreq_cpu_data);
 /* This one keeps track of the previously set governor of a removed CPU */
 struct cpufreq_cpu_save_data {
 	char gov[CPUFREQ_NAME_LEN];
-	unsigned int max, min, user_max, user_min;
+	unsigned int max, min;
 };
 static DEFINE_PER_CPU(struct cpufreq_cpu_save_data, cpufreq_policy_save);
 #endif
@@ -857,18 +857,14 @@ static int cpufreq_add_dev_policy(unsigned int cpu,
 	}
 	if (per_cpu(cpufreq_policy_save, cpu).min) {
 		policy->min = per_cpu(cpufreq_policy_save, cpu).min;
-		//policy->user_policy.min = policy->min;
-		policy->user_policy.min = per_cpu(cpufreq_policy_save, cpu).user_min;
+		policy->user_policy.min = policy->min;
 	}
 	if (per_cpu(cpufreq_policy_save, cpu).max) {
 		policy->max = per_cpu(cpufreq_policy_save, cpu).max;
-		//policy->user_policy.max = policy->max;
-		policy->user_policy.max = per_cpu(cpufreq_policy_save, cpu).user_max;
+		policy->user_policy.max = policy->max;
 	}
 	pr_debug("Restoring CPU%d min %d and max %d\n",
 		cpu, policy->min, policy->max);
-	pr_debug("Restoring CPU%d user_min %d and user_max %d\n",
-		cpu, policy->user_policy.min, policy->user_policy.max);
 #endif
 
 	for_each_cpu(j, policy->cpus) {
@@ -1222,14 +1218,8 @@ static int __cpufreq_remove_dev(struct sys_device *sys_dev)
 			CPUFREQ_NAME_LEN);
 	per_cpu(cpufreq_policy_save, cpu).min = data->min;
 	per_cpu(cpufreq_policy_save, cpu).max = data->max;
-        pr_debug("Saving CPU%d policy min %d and max %d\n",
-                        cpu, data->min, data->max);
-
-	per_cpu(cpufreq_policy_save, cpu).user_min = data->user_policy.min;
-	per_cpu(cpufreq_policy_save, cpu).user_max = data->user_policy.max;
-	pr_debug("Saving CPU%d policy user_min %d and user_max %d\n",
-			cpu, data->user_policy.min, data->user_policy.max);
-
+	pr_debug("Saving CPU%d policy min %d and max %d\n",
+			cpu, data->min, data->max);
 #endif
 
 	/* if we have other CPUs still registered, we need to unlink them,
@@ -1259,12 +1249,6 @@ static int __cpufreq_remove_dev(struct sys_device *sys_dev)
 			per_cpu(cpufreq_policy_save, j).max = data->max;
 			pr_debug("Saving CPU%d policy min %d and max %d\n",
 					j, data->min, data->max);
-
-                        per_cpu(cpufreq_policy_save, j).user_min = data->policy_user.min;
-                        per_cpu(cpufreq_policy_save, j).user_max = data->policy_user.max;
-                        pr_debug("Saving CPU%d policy user_min %d and user_max %d\n",
-                                        j, data->policy_user.min, data->policy_user.max);
-
 #endif
 			cpu_sys_dev = get_cpu_sysdev(j);
 			kobj = &cpu_sys_dev->kobj;
